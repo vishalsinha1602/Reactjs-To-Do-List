dc95556fdbdee50d8c8d6a840be63cb0fdd86f00
vishal_sinha <vishalsinha.inbox@gmail.com>
2025-06-28 01:06:10 +0530

    Adding tailwind configuration

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <h1 className="bg-red-500">hello</h1>
+  </StrictMode>
+);
